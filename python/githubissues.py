3b3c5033c62b9332f602eec3fa0c418c5ef3e6bc
ljodice <>
2023-03-13 18:06:54 -0400

    added python folder, added other scripts

---
+++ python/githubissues.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+# Enter your GitHub credentials
+username = "ljodiceendicott"
+password = "Minivinny1552?"
+
+# Enter the repository details
+owner = "ljodiceendicott"
+repo = "CrowdWatch"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "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"
+}
+# Enter the issue details
+issues = [
+    {"title": "Issue Title 1", "body": "Issue Description 1"},
+    {"title": "Issue Title 2", "body": "Issue Description 2"},
+    {"title": "Issue Title 3", "body": "Issue Description 3"}
+]
+
+for issue in issues:
+    # Make the API request to create the issue
+    url = f"https://api.github.com/repos/{owner}/{repo}/issues"
+    response = requests.post(url, auth=(username, password),headers=headers, json=issue)
+
+    # Print the response from the API
+    print(json.dumps(response.json(), indent=4))
